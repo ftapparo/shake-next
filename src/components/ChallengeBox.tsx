d1e3df4101580972f410ec90f9acb5a7968ddc1b
Flavio Eduardo Tapparo <tapparo.flavio@gmail.com>
2021-02-26 14:53:04 -0300

    Implemented the rest of features, including the contexts and components, and, finished all the css and appearance.

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+
+import styles from "../styles/components/ChallengeBox.module.css";
+
+export function ChallengeBox() {
+    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.challengeBoxContainer}>
+            {activeChallenge ? (
+                <div className={styles.challengeActive}>
+
+                    <header>Ganhe {activeChallenge.amount} xp</header>
+
+                    <main>
+                        <img src={`icons/${activeChallenge.type}`} alt={activeChallenge.type} />
+                        <strong>Novo desafio</strong>
+                        <p>{activeChallenge.description}</p>
+                    </main>
+
+                    <footer>
+
+                        <button type="button" className={styles.challengeFailedButton} onClick={resetChallenge}>
+                            Não completado
+                        </button>
+
+                        <button type="button" className={styles.challengeSucceededButton}>
+                            Completado
+                        </button>
+
+                    </footer>
+
+                </div>
+            ) : (
+                    <div className={styles.challengeNotActive}>
+                        <strong>Finalize um ciclo para receber um desafio</strong>
+                        <p>
+                            <img src="icons/level-up.svg" alt="Level up" />
+                            Avance de nível completando desafios.
+                        </p>
+                    </div>
+                )}
+        </div>
+    );
+}
